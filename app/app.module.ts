cc5616c1a43d03dbf716d2d92df67ce6d4bcb8ca
matejgeljic <32310104+matejgeljic@users.noreply.github.com>
2018-10-10 23:38:22 +0200

    Add files via upload

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { FlashMessagesModule } from 'angular2-flash-messages';
 
 import { environment } from '../environments/environment';
 import { AngularFireModule } from 'angularfire2';
@@ -44,7 +45,8 @@ import { ClientService } from '../app/services/client.service';
     AppRoutingModule,
     AngularFireModule.initializeApp(environment.firebase, 'clientpanel'),
     AngularFirestoreModule,
-    AngularFireAuthModule
+    AngularFireAuthModule,
+    FlashMessagesModule.forRoot()
   ],
   providers: [ClientService],
   bootstrap: [AppComponent]
--- app/components/add-client/add-client.component.ts
+++ app/components/add-client/add-client.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { FlashMessagesService } from 'angular2-flash-messages';
+import { ClientService } from '../../services/client.service';
+import { Router } from '@angular/router';
 
 import { Client } from '../../models/Client';
 
@@ -17,10 +20,32 @@ export class AddClientComponent implements OnInit {
   }
 
   disableBalanceOnAdd: boolean = true;
+  @ViewChild('clientForm') form: any;
 
-  constructor() { }
+  constructor(private flashMessage: FlashMessagesService, private clientService: ClientService, private router: Router) { }
 
   ngOnInit() {
+  }
+
+  onSubmit({value, valid}: {value: Client, valid: boolean}) {
+    if(this.disableBalanceOnAdd) {
+      value.balance = 0;
+    }
+
+    if(!valid) {
+      this.flashMessage.show('Please fill out the form correctly', {
+        cssClass: 'alert-danger', timeout: 3000
+      });
+    } else {
+      //Add new client
+      this.clientService.newClient(value);
+      //Show Message
+      this.flashMessage.show('New client added', {
+        cssClass: 'alert-success', timeout: 3000
+      });
+      //Redirect to dashboard
+      this.router.navigate(['/']);
+    }
   }
 
 }
